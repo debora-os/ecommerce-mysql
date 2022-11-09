36ed80c5ea7d062e93bbc999d008c5ca62b69e27
debora_o_s <debruxo31@gmail.com>
2022-11-09 17:23:27 -0300

    model user

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const db = require("./models/db");
+const User = require("./models/User");
+
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-    res.send("Página inicial")
+  res.send("Página inicial");
 });
 app.post("/cadastrar", async (req, res) => {
-    res.send("Página inicial")
+
+  await User.create(req.body)
+
+    .then(() => {
+      return res.json({
+        erro: false,
+        mensagem: "Usuario Cadastrado",
+      });
+    })
+    .catch(() => {
+        return res.status(400).json({
+            erro: true,
+            mensagem: "Erro: Usuario não Cadastrado",
+          });
+    });
+
 });
 
 app.listen(3000, () => {
-    console.log("Server in port 3000: http://localhost:3000");
-})
+  console.log("Server in port 3000: http://localhost:3000");
+});
